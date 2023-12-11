d677301335b818cb68b2b045822285e5553d5b2a
Joaquim-Claudio <116195116+Joaquim-Claudio@users.noreply.github.com>
2023-12-11 03:54:30 +0000

    Updated Main Activity and Created Note Activity.

---
+++ App/app/src/main/java/pt/iade/joaquimclaudio/atividade/NoteActivity.java
@@ -0,0 +1,14 @@
+package pt.iade.joaquimclaudio.atividade;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NoteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_note);
+    }
+}
---
+++ App/app/src/main/java/pt/iade/joaquimclaudio/atividade/models/NoteItem.java
@@ -0,0 +1,10 @@
+package pt.iade.joaquimclaudio.atividade.models;
+
+import java.util.GregorianCalendar;
+
+public class NoteItem {
+    private String title;
+    private String content;
+    private GregorianCalendar creationDate;
+    private GregorianCalendar modifiedDate;
+}
